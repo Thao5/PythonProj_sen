4422b4bcea22c627fb47a116739fde98106404f8
quocthao9899@gmail.com <quocthao9899@gmail.com>
2024-06-29 00:31:36 +0700

    update

--- system_recomendation.py
+++ system_recomendation.py
@@ -9,7 +9,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report
-import openpyxl
 import pandas as pd
 import numpy as np
 
